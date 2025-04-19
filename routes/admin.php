14c2260064f7da8bdc6509a9be2eff7c0217d8d6
Hesam <hesam.15ztr@gmail.com>
2025-04-19 19:27:57 +0330

    create admin.php to store the admin panel routes

---
+++ routes/admin.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Services\DatePicker;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckServiceCenter;
+use App\Http\Controllers\DashboardController;
+
+require __DIR__.'/web/beforeCompleteLogin.php';
+
+Route::middleware(['auth', 'verified', CheckServiceCenter::class, 'role:adminstrator|expert|clerk'])->group(function () {    
+    Route::prefix('dashboard')->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('home');
+
+        //Service Center
+        require __DIR__.'/web/serviceCenter.php';
+
+        //Users
+        require __DIR__.'/web/user.php';
+
+        //Customers
+        require __DIR__.'/web/customer.php';
+
+        //Bookings
+        require __DIR__.'/web/booking.php';
+
+        //Cars
+        require __DIR__.'/web/car.php';
+
+        //Reports
+        require __DIR__.'/web/report.php';
+
+        //Options
+        require __DIR__.'/web/option.php';
+
+        Route::get('/datepicker-settings', [DatePicker::class, 'getSettings']);
+        Route::get('/available-times', [DatePicker::class, 'getAvailableTimes']);
+    });
+});
